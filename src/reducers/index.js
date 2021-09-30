b77c1a9a615a298dff2b3476f9fde242d6c33ef3
mohd5289 <mohd5289@yahoo.com>
2021-09-30 14:28:13 +0100

    Created Add Project Task form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import backlogReducer from "./backlogReducer";
 import errorReducer from "./errorReducer";
 import productReducer from "./productReducer";
 
 export default combineReducers({
   errors: errorReducer,
-  projects:productReducer
+  projects:productReducer,
+  backlog:backlogReducer
 });
