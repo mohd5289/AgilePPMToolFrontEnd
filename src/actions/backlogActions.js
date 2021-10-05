1a5968381f530e045cb40d1c755e457891a8a833
mohd5289 <mohd5289@yahoo.com>
2021-10-05 12:27:24 +0100

    More Commits

---
+++ src/actions/backlogActions.js
@@ -0,0 +1,32 @@
+import axios from "axios"
+import{GET_BACKLOG, GET_ERRORS} from "./types";
+
+export const addProjectTask=(backlog_id,project_task,history)=>async dispatch=>{
+try{
+    await axios.post(`http://localhost:8050/api/backlog/${backlog_id}`,project_task);
+
+history.push(`/projectBoard/${backlog_id}`)
+dispatch({
+    type: GET_ERRORS,
+    payload: {}
+  });
+}catch(err){
+    dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+}
+}
+
+export const getBacklog= backlog_id => async dispatch =>{
+    try{
+        const res = await axios.get(`http://localhost:8050/api/backlog/${backlog_id}`);
+    dispatch({
+        type:GET_BACKLOG,
+        payload: res.data
+    });
+    
+    }catch(err){
+        
+    }
+} 
